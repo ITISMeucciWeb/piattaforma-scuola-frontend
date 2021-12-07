4c035de7be82e2c49728f59d32055a324070693d
Lorenzo Rizzotti <dev@dreaming.codes>
2021-12-08 00:59:06 +0100

    Authors.vue

--- src/views/landing/Authors.vue
+++ src/views/landing/Authors.vue
@@ -6,7 +6,28 @@
 
 <script>
 export default {
-  name: "Authors"
+  name: "Authors",
+  data: ()=> {
+    return{
+      authors: [
+        {
+          image: 'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+          name: "Lorenzo",
+          lastname: "Rizzotti",
+          nickname: 'DreamingCodes',
+          class: "3C-IA",
+          website: "https://dreaming.codes",
+          chips: [
+            {
+              color: 'blue',
+              text: 'Sognante',
+              icon: 'mdi-bed'
+            }
+          ]
+        },
+      ],
+    }
+  }
 }
 </script>
 
