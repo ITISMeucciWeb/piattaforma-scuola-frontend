b0ad3be48572da3fb5362ec7fcd21ece943ba8b0
Lorenzo Rizzotti <dev@dreaming.codes>
2021-12-10 23:45:29 +0100

    Changes by Tommaso

--- src/views/landing/Privacy.vue
+++ src/views/landing/Privacy.vue
@@ -1,7 +1,144 @@
 <template>
-  <div>
-
-  </div>
+  <v-container fluid >
+    <v-card elevation="6" class="mx-auto" width="80%">
+      <v-card-title>Privacy Policy</v-card-title>
+      <v-card-text>
+        <td class="d-block comment-body markdown-body  js-comment-body">
+          <h3 dir="auto">NORMATIVA SULLA PRIVACY</h3>
+          <blockquote>
+            <p dir="auto">Informativa ai sensi dell'art. 13 del Codice della Privacy</p>
+          </blockquote>
+          <p dir="auto"><strong>Ai sensi dell'articolo 13 del codice della D.Lgs. 196/2003, vi rendiamo presenti le
+            seguenti informazioni.</strong></p>
+          <p dir="auto">Noi di piattaformameucci.com riteniamo che la privacy dei nostri visitatori sia estremamente
+            importante. Questo documento descrive dettagliatamente i tipi di informazioni personali raccolti e
+            registrati dal nostro sito e come essi vengano utilizzati.</p>
+          <h3 dir="auto">File di Registrazione (Log Files)</h3>
+          <p dir="auto">Come molti altri siti web, il nostro utilizza file di log. Questi file registrano
+            semplicemente i visitatori del sito - di solito una procedura standard delle aziende di hosting e dei
+            servizi di analisi degli hosting.</p>
+          <p dir="auto">Le informazioni contenute nei file di registro comprendono indirizzi di protocollo Internet
+            (IP), il tipo di browser, Internet Service Provider (ISP), informazioni come data e ora, pagine
+            referral,
+            pagine d'uscita ed entrata o il numero di clic.
+            Queste informazioni vengono utilizzate per analizzare le tendenze, amministrare il sito, monitorare il
+            movimento degli utenti sul sito e raccogliere informazioni demografiche. Gli indirizzi IP e le altre
+            informazioni non sono collegate a informazioni personali che possono essere identificate, <strong>dunque
+              tutti i dati sono raccolti in forma assolutamente anonima.</strong></p>
+          <h3 dir="auto">Questo sito web utilizza i Cookies</h3>
+          <p dir="auto">I cookies sono piccoli file di testo che vengono automaticamente posizionati sul PC del
+            navigatore all’interno del browser. Essi contengono informazioni di base sulla navigazione in Internet e
+            grazie al browser vengono riconosciuti ogni volta che l’utente visita il sito.</p>
+          <h3 dir="auto">Cookie Policy</h3>
+          <p dir="auto">Questo sito utilizza cookies, anche di terze parti, per migliorarne l’esperienza di
+            navigazione, consentire a chi naviga di usufruire di eventuali servizi online e monitorare la
+            navigazione
+            nel sito.</p>
+          <h3 dir="auto">Gestione dei Cookies</h3>
+          <p dir="auto">I cookies utilizzati in questo sito possono rientrare nelle categorie descritte di
+            seguito.</p>
+          <ul dir="auto">
+            <li>Attività strettamente necessarie al funzionamento.</li>
+          </ul>
+          <blockquote>
+            <p dir="auto">Questi cookies hanno natura tecnica e permettono al sito di funzionare correttamente. Ad
+              esempio, mantengono l’utente collegato durante la navigazione evitando che il sito richieda di
+              collegarsi più volte per accedere alle pagine successive.</p>
+          </blockquote>
+          <ul dir="auto">
+            <li>Attività di salvataggio delle preferenze</li>
+          </ul>
+          <blockquote>
+            <p dir="auto">Questi cookie permettono di ricordare le preferenze selezionate
+              dall’utente durante la navigazione, ad esempio, consentono di
+              impostare la lingua.</p>
+          </blockquote>
+          <h3 dir="auto">Norme sulla privacy di terze parti</h3>
+          <p dir="auto">È necessario consultare le rispettive norme sulla privacy di questi server di terze parti
+            per
+            ulteriori informazioni sulle loro pratiche e per istruzioni su come disattivare alcune pratiche.</p>
+          <p dir="auto">La nostra politica sulla privacy non si applica ai fornitori di terze parti ed ai partner
+            pubblicitari, e non possiamo controllare le attività di tali altri inserzionisti o siti web.</p>
+          <p dir="auto">Se desideri disattivare i cookie, puoi farlo attraverso le tue singole opzioni del browser.
+            Ulteriori informazioni sulla gestione dei cookie con browser web specifico possono essere trovati nei
+            rispettivi siti web dei browser</p>
+          <h3 dir="auto">Finalità del trattamento</h3>
+          <p dir="auto">I dati possono essere raccolti per la seguente finalità:</p>
+          <ul dir="auto">
+            <li>eseguire gli obblighi previsti da leggi o regolamenti;</li>
+            <li>gestione contatti;</li>
+            <li>fornire l'accesso ad aree riservate del Portale e di Portali/siti
+              collegati con il presente e all'invio di comunicazioni anche di
+              carattere commerciale, notizie, aggiornamenti sulle iniziative di
+              questo sito e delle società da essa controllate e/o collegate e/o
+              Sponsor.
+            </li>
+          </ul>
+          <h3 dir="auto">Modalità del trattamento</h3>
+          <ul dir="auto">
+            <li>I dati verranno trattati con le seguenti modalità:</li>
+            <li>raccolta dati con modalità single-opt, in apposito database;</li>
+            <li>registrazione ed elaborazione su supporto cartaceo e/o magnetico;</li>
+            <li>organizzazione degli archivi in forma prevalentemente automatizzata,
+              ai sensi del Disciplinare Tecnico in materia di misure minime di
+              sicurezza, Allegato B del Codice della Privacy.
+            </li>
+          </ul>
+          <h3 dir="auto">Natura obbligatoria</h3>
+          <p dir="auto">Tutti i dati richiesti sono obbligatori.</p>
+          <h3 dir="auto">Diritti dell'interessato</h3>
+          <p dir="auto">Ai sensi ai sensi dell'art. 7 (Diritto di accesso ai dati personali ed altri diritti) del
+            Codice della Privacy, vi segnaliamo che i vostri diritti in ordine al trattamento dei dati sono:</p>
+          <ul dir="auto">
+            <li>
+              <p dir="auto">conoscere, mediante accesso gratuito l'esistenza di trattamenti di
+                dati che possano riguardarvi;</p>
+            </li>
+            <li>
+              <p dir="auto">essere informati sulla natura e sulle finalità del trattamento</p>
+            </li>
+            <li>
+              <p dir="auto">ottenere a cura del titolare, senza ritardo:</p>
+              <ul dir="auto">
+                <li>
+                  <p dir="auto">la conferma dell'esistenza o meno di dati personali che vi
+                    riguardano, anche se non ancora registrati, e la comunicazione in
+                    forma intellegibile dei medesimi dati e della loro origine, nonché
+                    della logica e delle finalità su cui si basa il trattamento; la
+                    richiesta può essere rinnovata, salva l'esistenza di giustificati
+                    motivi, con intervallo non minore di novanta giorni;</p>
+                </li>
+                <li>
+                  <p dir="auto">la cancellazione, la trasformazione in forma anonima o il blocco dei
+                    dati trattati in violazione di legge, compresi quelli di cui non è
+                    necessaria la conservazione in relazione agli scopi per i quali i
+                    dati sono stati raccolti o successivamente trattati;</p>
+                </li>
+                <li>
+                  <p dir="auto">l'aggiornamento, la rettifica ovvero, qualora vi abbia interesse,
+                    l'integrazione dei dati esistenti;</p>
+                </li>
+                <li>
+                  <p dir="auto">opporvi in tutto o in parte per motivi legittimi al trattamento dei
+                    dati personali che vi riguardano ancorché pertinenti allo scopo della
+                    raccolta;</p>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <p dir="auto">Per esercitare i diritti previsti all'art. 7 del Codice della Privacy ovvero per la
+            cancellazione dei vostri dati dall'archivio, è sufficiente contattarci attraverso uno dei canali messi a
+            disposizione. Tutti i dati sono protetti attraverso l'uso di antivirus, firewall e protezione attraverso
+            password.</p>
+          <h3 dir="auto">Consenso</h3>
+          <p dir="auto">Usando il nostro sito web, acconsenti alla nostra politica sulla privacy e accetti i suoi
+            termini. Se desideri ulteriori informazioni o hai domande sulla nostra politica sulla privacy non
+            esitare
+            a contattarci.</p>
+        </td>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -10,6 +147,4 @@ export default {
 }
 </script>
 
-<style scoped>
 
-</style>
