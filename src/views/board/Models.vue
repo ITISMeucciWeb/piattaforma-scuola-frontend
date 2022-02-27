b47e169566536bb7dd948d4cfbd781a06180679d
DreamingCodes <dev@dreaming.codes>
2022-02-27 21:31:44 +0100

    Removed pdf-lib

--- src/views/board/Models.vue
+++ src/views/board/Models.vue
@@ -1,19 +1,15 @@
 <template>
   <v-container fluid class="fill-height pa-0">
     <v-card width="100%" height="500" elevation="12" color="other" @dragenter.prevent @dragover.prevent @drop.prevent="handleModelDrop">
-      <PDFFormsBuilder></PDFFormsBuilder>
     </v-card>
-
   </v-container>
 </template>
 
 <script lang="ts">
-import {PDFDocument} from "pdf-lib";
-import PDFFormsBuilder from "@/components/PDFFormsBuilder.vue";
+
 
 export default {
   name: "Modelli-admin",
-  components: {PDFFormsBuilder},
   methods: {
     async handleModelDrop(e: DragEvent) {
       //const test = await PDFDocument.load(await e.dataTransfer.files[0].arrayBuffer());
