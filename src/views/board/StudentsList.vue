2875a2ca8993a697d94b7d8a5056ff5cd67f9d1a
DreamingCodes <dev@dreaming.codes>
2022-03-07 10:29:54 +0100

    Fixed elastic search pagination

--- src/views/board/StudentsList.vue
+++ src/views/board/StudentsList.vue
@@ -1,7 +1,21 @@
 <template>
   <v-container fluid class="fill-height pa-0">
     <v-card width="100%" elevation="12" color="other">
-      <v-data-table class="other" virt :items="students" :items-per-page="15"></v-data-table>
+      <v-data-table ref="dataTable" class="other" virt :headers="tableHeaders" v-on:update:options="changeOptions"
+                    :page.sync="page"
+                    :footer-props="{
+                      'items-per-page-options': [5, 10, 15, 20]
+                    }"
+                    :items="getUsersByName.users" :items-per-page="15" :server-items-length="getUsersByName.count">
+        <template v-slot:top>
+          <v-text-field @keydown="prepareForSearch" v-model="nameSearch" append-icon="mdi-magnify"></v-text-field>
+        </template>
+        <template v-slot:[`item.hasDisorders`]="{ item }">
+          <v-icon>
+            {{ item.hasDisorders ? "mdi-checkbox-marked" : "mdi-checkbox-blank-outline" }}
+          </v-icon>
+        </template>
+      </v-data-table>
     </v-card>
   </v-container>
 </template>
@@ -14,12 +28,19 @@ import gql from "graphql-tag";
 export default {
   name: "Students-list",
   apollo: {
-    students: {
+    getUsersByName: {
       query: gql`
         query getUsersByName($from: Int, $limit: Int, $nameSearch: String!){
           getUsersByName(from: $from, limit: $limit, nameSearch: $nameSearch){
-          name
-          surname
+            count
+            users {
+              name
+              surname
+              class
+              division
+              role
+              hasDisorders
+            }
           }
         }`,
       variables() {
@@ -36,9 +57,53 @@ export default {
       nameSearch: "",
       limit: 15,
       from: 0,
+      page: 1,
+      tableHeaders: [
+        {
+          text: "Name",
+          value: "name",
+          sortable: false,
+        },
+        {
+          text: "Surname",
+          value: "surname",
+          sortable: false,
+        },
+        {
+          text: "Ruolo",
+          value: "role",
+          sortable: false,
+        },
+        {
+          text: "Ha disorders",
+          value: "hasDisorders",
+          sortable: false,
+        },
+        {
+          text: "Classe",
+          value: "class",
+          sortable: false,
+        },
+        {
+          text: "Sezione",
+          value: "division",
+          sortable: false,
+        }
+      ]
     };
   },
   methods: {
+    logData(data){
+      console.log(data);
+    },
+    prepareForSearch() {
+      this.page = 1;
+      console.log("sdsa", this.page);
+    },
+    changeOptions(options: any){
+      this.from = (options.page - 1) * options.itemsPerPage;
+      this.limit = options.itemsPerPage;
+    }
   }
 }
 </script>
